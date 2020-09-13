9893d3b91994129a5e49175056fce6ab9b412173
Radik Zangirov <owtmpatrick@gmail.com>
2020-09-13 13:59:32 +0500

    added possiility to use keyboard

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,18 +1,17 @@
 export {};
+import replaceCharInExpression from '../utils/replace-char-in-expression';
+import getElementText from '../utils/get-element-text';
 
-const replaceCharinExpression = (
-	expression: string,
-	initialChar: string,
-	replacedChar: string
-): string => expression.split(initialChar).join(replacedChar);
+import keys from '../constants/keys';
 
-let split = (expression: string, operator: string) => {
+const split = (expression: string, operator: string) => {
 	const result = [];
 	let braces = 0;
 	let currentChunk = '';
 
 	for (let i = 0; i < expression.length; ++i) {
 		const curCh = expression[i];
+
 		if (curCh === '(') {
 			braces++;
 		} else if (curCh === ')') {
@@ -31,11 +30,14 @@ let split = (expression: string, operator: string) => {
 	return result;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const parseDivisionSeparatedExpression = (expression: string): any => {
 	const numbersString = split(expression, '/');
 	const numbers = numbersString.map(noStr => {
 		if (noStr[0] === '(') {
 			const expr = noStr.substr(1, noStr.length - 2);
+
+			// eslint-disable-next-line no-use-before-define
 			return parsePlusSeparatedExpression(expr);
 		}
 
@@ -54,13 +56,15 @@ const parseDivisionSeparatedExpression = (expression: string): any => {
 	return result;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const parseMultiplicationSeparatedExpression = (expression: string): any => {
-	const numbersString = split(replaceCharinExpression(expression, '×', '*'), '*');
+	const numbersString = split(replaceCharInExpression(expression, '×', '*'), '*');
 	const numbersDivision = numbersString.map(noStr => parseDivisionSeparatedExpression(noStr));
 	const numbers = numbersDivision.map(noStr => {
 		if (noStr[0] === '(') {
 			const expr = noStr.substr(1, noStr.length - 2);
 
+			// eslint-disable-next-line no-use-before-define
 			return parsePlusSeparatedExpression(expr);
 		}
 
@@ -95,31 +99,61 @@ const btns = [].slice.call(document.querySelectorAll<HTMLButtonElement>('.calc__
 const resultBtn = document.querySelector('.calc__btn_result') as HTMLButtonElement;
 const inputIn = document.querySelector('.calc__input_in') as HTMLDivElement;
 const inputOut = document.querySelector('.calc__input_out') as HTMLDivElement;
-
-const getBtntext = (button: any): string => button.textContent;
+const hiddenInput = document.querySelector('.calc__hidden-input') as HTMLInputElement;
 
 const parse = (): void => {
-	const expression = replaceCharinExpression(getBtntext(inputIn), ',', '.');
+	const expression = replaceCharInExpression(getElementText(inputIn), ',', '.');
+
+	if (!expression) {
+		return;
+	}
+
 	const result = parsePlusSeparatedExpression(expression);
 
 	inputOut.textContent = result;
 };
 
+const onKeyDown = (event: KeyboardEvent): void => {
+	const {key} = event;
+
+	if (keys.find((k: string) => k === key)) {
+		if (key === 'Enter') {
+			parse();
+			return;
+		}
+
+		if (key === '*') {
+			inputIn.textContent += '×';
+			return;
+		}
+
+		inputIn.textContent += key;
+	}
+};
+
 btns.forEach((btn: HTMLButtonElement): void => {
 	btn.addEventListener('click', (): void => {
-		const text: string = getBtntext(btn);
+		const text: string = getElementText(btn);
 
 		if (text === '=') {
+			hiddenInput.focus();
 			return;
 		}
 
 		if (text === 'C') {
 			inputIn.textContent = '';
+			hiddenInput.focus();
 			return;
 		}
 
 		inputIn.textContent += text;
+		hiddenInput.focus();
 	});
 });
 
 resultBtn.addEventListener('click', parse);
+hiddenInput.addEventListener('keydown', onKeyDown);
+
+window.addEventListener('load', (): void => hiddenInput.focus());
+
+document.addEventListener('click', (): void => hiddenInput.focus());
---
+++ src/constants/keys.ts
@@ -0,0 +1,22 @@
+const availableKeys: string[] = [
+	'0',
+	'1',
+	'2',
+	'3',
+	'4',
+	'5',
+	'6',
+	'7',
+	'8',
+	'9',
+	'+',
+	'-',
+	'*',
+	'/',
+	',',
+	')',
+	'(',
+	'Enter'
+];
+
+export default availableKeys;
