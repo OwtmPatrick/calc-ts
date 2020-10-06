ef7fef87fcae5ce2de5b67e5e918d8ea00d6008f
owtmpatrick <owtmpatrick@gmail.com>
2020-10-06 21:24:45 +0500

    added nulls omitting

--- src/tests/parseExpression.ts
+++ src/tests/parseExpression.ts
@@ -1,6 +1,7 @@
 import parsePlusSeparatedExpression from '../app/parse-expression';
+import omitNules from '../utils/omit-nules';
 
-test('parsing plus expresison', () => {
+test('parsing plus expresison', (): void => {
 	expect(parsePlusSeparatedExpression('37+25')).toBe(62);
 });
 
@@ -14,4 +15,16 @@ test('parsing multiplication expresison', () => {
 
 test('parsing expression with brackets', () => {
 	expect(parsePlusSeparatedExpression('(12+4)*21-2*(16/4)')).toBe(328);
+});
+
+test('should return the same value if there are not nulls', () => {
+	expect(omitNules('54.1245')).toBe('54.1245');
+});
+
+test('should remove all the null before dot', () => {
+	expect(omitNules('54.124500000000')).toBe('54.1245');
+});
+
+test('should not remove null directly before dot', () => {
+	expect(omitNules('54.0')).toBe('54.0');
 });
---
+++ src/utils/omit-nules.ts
@@ -0,0 +1,12 @@
+export default function omitNules(str: string): string {
+	const lastChar = str[str.length - 1];
+	const preLastChar = str[str.length - 2];
+
+	if (lastChar === '0' && (preLastChar === '0' || preLastChar !== '.')) {
+		const newStr = str.substring(0, str.length - 1);
+
+		return omitNules(newStr);
+	}
+
+	return str;
+}
